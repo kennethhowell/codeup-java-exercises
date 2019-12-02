262dabab23bc551fb16119b9204a55723cfb6822
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-12-02 11:25:03 -0600

    exer 1 complete

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,10 @@
 public class Person {
     private String name;
 
+    public static String getName(Person personname) {
+        return personname.name;
+    }
+
     public String getName(){
         return this.name;
     };
@@ -21,27 +25,27 @@ public class Person {
     }
 
     public static void main(String[] args){
-//        Person ken = new Person("Ken");
+        Person ken = new Person("Ken");
 
 //        Person person1 = new Person("John");
 //        Person person2 = new Person("John");
 //        System.out.println(person1.getName().equals(person2.getName()));
 //        System.out.println(person1 == person2);
-
+//
 //        Person person1 = new Person("John");
 //        Person person2 = person1;
 //        System.out.println(person1 == person2);
 
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
-//        person2.setName("Jane");
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
-//        person1.setName("Robocop");
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person1.setName("Robocop");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
 
     }
 }
