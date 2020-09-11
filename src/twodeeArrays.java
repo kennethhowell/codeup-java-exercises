77ce1a2c163043afa59f33af819da1880fad89c7
kennethhowell <howell.kenneth.a@gmail.com>
2020-09-11 12:27:25 -0500

    set up 2d array print system - math.random range -9 to 9 > printing and filling up 2d array properly > meat of the solve, grab 'hourglasses' as summed sets next step

--- src/twodeeArrays.java
+++ src/twodeeArrays.java
@@ -1,6 +1,43 @@
 public class twodeeArrays {
-    int rowSize= 2;
-    int colSize = 4;
-    int [][]myArray = new int[rowSize][colSize];
-    int count = 0;
+//    int rowSize= 2;
+//    int colSize = 4;
+//    int [][]myArray = new int[rowSize][colSize];
+//    int count = 0;
+
+    static void arrayPrint(int rowSize,int colSize,int[][]myArray){
+//        int min = 9;
+//        int max = 9;
+//        int range = max + min;
+//
+//        int count = (int) (Math.random() * 9) - 9;
+
+        for(int i = 0; i < rowSize; i++) {
+
+            for(int j = 0; j < colSize; j++) {
+                int count = (int) (Math.random() * 18) - 9;
+                System.out.println("count = " + count);
+                myArray[i][j] = count;
+            }
+        }
+
+
+        for (int i = 0; i < rowSize; i++){
+
+            for(int j = 0; j < colSize; j++){
+                System.out.print(myArray[i][j] +" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int rowSize= 6;
+        int colSize = 6;
+        int [][]myArray = new int[rowSize][colSize];
+
+
+
+        arrayPrint(rowSize, colSize, myArray);
+    }
+
 }
